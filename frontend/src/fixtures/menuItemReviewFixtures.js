48380dd7347422b6e5fcdf1374cc0c11c61c270b
KushagraKanaujia <kushagrakanaujia@umail.ucsb.edu>
2024-02-14 19:52:28 -0800

    kk - Create menuItemReviewFixtures.js

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,45 @@
+const menuItemReviewFixtures = {
+    oneReview:
+    [
+      {
+       "id": 1,
+        "itemId": 4,
+        "stars": 3,
+        "reviewerEmail": "kush13@ucsb.edu",
+        "dateReviewed": "2024-01-03T00:00:00",
+        "comments": "First Comment"
+      }
+    ],
+
+    threeReviews:
+    [
+        {
+            "id": 2,
+            "itemId": 5,
+            "stars": 4,
+            "reviewerEmail": "kush@ucsb.edu",
+            "dateReviewed": "2024-01-03T00:00:02",
+            "comments": "Second Comment"
+        },
+
+        {
+            "id": 3,
+            "itemId": 5,
+            "stars": 3,
+            "reviewerEmail": "kush1309@ucsb.edu",
+            "dateReviewed": "2024-01-03T00:00:5",
+            "comments": "Third Comment"
+        },
+
+        {
+            "id": 4,
+            "itemId": 6,
+            "stars": 2,
+            "reviewerEmail": "kush2013@ucsb.edu",
+            "dateReviewed": "2024-01-03T00:03:02",
+            "comments": "Fourth Comment"
+        },
+    ]
+};
+
+export { menuItemReviewFixtures };
