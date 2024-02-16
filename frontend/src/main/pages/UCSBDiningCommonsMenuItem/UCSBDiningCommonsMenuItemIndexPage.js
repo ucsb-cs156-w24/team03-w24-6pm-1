692359140352964d9d3f80e1b52933d653ae1153
Philly <phillytlim@gmail.com>
2024-02-15 17:51:19 -0800

    finished index page for menu items

--- frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemIndexPage.js
+++ frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemIndexPage.js
@@ -1,15 +1,45 @@
+import React from 'react'
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+import { useBackend } from 'main/utils/useBackend';
+import UCSBDiningCommonsMenuItemTable from 'main/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemTable';
+import { useCurrentUser , hasRole} from 'main/utils/currentUser'
+import { Button } from 'react-bootstrap';
 
 export default function UCSBDiningCommonsMenuItemIndexPage() {
 
-  // Stryker disable all : placeholder for future implementation
+  const currentUser = useCurrentUser();
+
+  const { data: menu, error: _error, status: _status } =
+      useBackend(
+          // Stryker disable next-line all : don't test internal caching of React Query
+          ["/api/ucsbdiningcommonsmenuitems/all"],
+          { method: "GET", url: "/api/ucsbdiningcommonsmenuitems/all" },
+          // Stryker disable next-line all : don't test default value of empty list
+          []
+      );
+
+  const createButton = () => {
+      if (hasRole(currentUser, "ROLE_ADMIN")) {
+          return (
+              <Button
+                  variant="primary"
+                  href="/ucsbdiningcommonsmenuitems/create"
+                  style={{ float: "right" }}
+              >
+                  Create UCSB Dining Commons Menu Item
+              </Button>
+          )
+      } 
+  }
+
   return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p><a href="/placeholder/create">Create</a></p>
-        <p><a href="/placeholder/edit/1">Edit</a></p>
-      </div>
-    </BasicLayout>
-  )
+      <BasicLayout>
+          <div className="pt-2">
+              {createButton()}
+              <h1>UCSB Dining Commons Menu</h1>
+              <UCSBDiningCommonsMenuItemTable menu={menu} currentUser={currentUser} />
+          </div>
+      </BasicLayout>
+  );
 }
