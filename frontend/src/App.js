391f7393db7768bd655c2424150d6edcf9869835
Shashank Bhagwani <shashank790@ucsb.edu>
2024-02-18 17:16:57 -0800

    sb removed placeholder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,10 +18,6 @@ import RestaurantCreatePage from "main/pages/Restaurants/RestaurantCreatePage";
 import RestaurantEditPage from "main/pages/Restaurants/RestaurantEditPage";
 
 
-import PlaceholderIndexPage from "main/pages/Placeholder/PlaceholderIndexPage";
-import PlaceholderCreatePage from "main/pages/Placeholder/PlaceholderCreatePage";
-import PlaceholderEditPage from "main/pages/Placeholder/PlaceholderEditPage";
-
 import ArticlesIndexPage from 'main/pages/Articles/ArticlesIndexPage';
 import ArticlesCreatePage from 'main/pages/Articles/ArticlesCreatePage';
 import ArticlesEditPage from 'main/pages/Articles/ArticlesEditPage';
